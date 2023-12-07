4f715e0717de7ae1e434a4abf0f936bba291a0e8
karthik <challakarthik@gmail.com>
2023-12-07 11:20:25 -0500

    first commit

---
+++ src/main/java/com/luv2code/ecommerce/entity/ProductCategory.java
@@ -0,0 +1,21 @@
+package com.luv2code.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Table(name="product_category")
+@Getter
+@Setter
+public class ProductCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    private String categoryName;
+    private Set<Product> products;
+}
